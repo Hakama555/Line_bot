7aaf254f30e2f653dc1dd2c599b55a67bbcfadf4
Hakama555 <54787616+Hakama555@users.noreply.github.com>
2021-07-16 14:52:40 +0900

    update

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import flask
-import numpy
+from flask import Flask, request, abort
+
+from linebot import (\
+    LineBotApi, WebhookHandler\
+        )
+from linebot.exceptions import (\
+        InvalidSignatureError\
+            )
+from linebot.models import (\
+        MessageEvent, TextMessage, TextSendMessage\
+            )
+import os
