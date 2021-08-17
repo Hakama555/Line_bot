bc4b5436e00b95eaaa7940e666315b399f54251a
Hakama555 <54787616+Hakama555@users.noreply.github.com>
2021-08-17 18:55:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-from flask import Flask, request, abort
+import requests
 
-from linebot import (\
-    LineBotApi, WebhookHandler\
-        )
-from linebot.exceptions import (\
-        InvalidSignatureError\
-            )
-from linebot.models import (\
-        MessageEvent, TextMessage, TextSendMessage\
-            )
-import os
-
-mes = None
-def message(mes):
-    mes = request.get_data()
-    return mes
-
-print(message())
+print("Hello World")
